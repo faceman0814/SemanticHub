ae5d4de48740ee551c192dc2a7fdc1fd57d717f9
faceman <1002784867@qq.com>
2024-04-07 09:35:16 +0800

    提交

---
+++ FaceMan.SemanticHub.Test/DataSet/DataSetTest.cs
@@ -0,0 +1,32 @@
+﻿using DocumentFormat.OpenXml.ExtendedProperties;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+using FaceMan.SemanticHub.KernelMemory;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceMan.SemanticHub.Test.DataSet
+{
+    [TestClass]
+    public class DataSetTest
+    {
+        [TestMethod]
+        public async Task GetChatMessageContentsAsync()
+        {
+            var chatgpt = new KernelMemoryService();
+            var _memory = chatgpt.GetMemoryByKMS();
+            var text = "苏州市燃气管理办法 \r\n（2018 年 4月23日苏州市人民政府令第145号发布；https://minio.com           \t  \r\n2020 年 4 月21日苏州市人民政府令第153号第一次修正，  \r\n自2018年6月1日起施行） \r\n第一章  总  则 \r\n第一条  为了加强燃气管理，保障燃气供应，规范燃气\r\n的经营和使用，保障公民生命、财产安全和公共安全，维护\r\n燃气用户和燃气经营企业的合法权益，根据《中华人民共和\r\n国安全生产法》、国务院《城镇燃气管理条例》《江苏省燃\r\n气管理条例》等有关法律、法规，结合本市实际";
+            await _memory.ImportTextAsync(text);
+            var results = await _memory.SearchAsync("苏州市人民政府", "knowledgeBaseTest");
+            //返回生成的文档摘要
+            foreach (var item in results.Results)
+            {
+                Console.WriteLine($"== {item.SourceName} summary ==\n{item.Partitions.First().Text}\n");
+            }
+        }
+    }
+}
---
+++ FaceMan.SemanticHub/KernelMemory/KernelMemoryService.cs
@@ -0,0 +1,67 @@
+﻿using FaceMan.SemanticHub.HttpClients;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.Configuration;
+using Microsoft.KernelMemory.ContentStorage.DevTools;
+using Microsoft.KernelMemory.FileSystem.DevTools;
+
+namespace FaceMan.SemanticHub.KernelMemory
+{
+    [ServiceDescription(typeof(IKernelMemoryService), ServiceLifetime.Singleton)]
+    public class KernelMemoryService : IKernelMemoryService
+    {
+        private MemoryServerless _memory;
+        public MemoryServerless GetMemoryByKMS(SearchClientConfig searchClientConfig = null)
+        {
+            //if (_memory.IsNull())
+            {
+
+                //http代理
+                var chatHttpClient = OpenAIHttpClientHandlerUtil.GetHttpClient("https://yoyochatx.openai.azure.com/");
+                var embeddingHttpClient = OpenAIHttpClientHandlerUtil.GetHttpClient("https://yoyochatx.openai.azure.com/");
+
+                //搜索配置
+                if (searchClientConfig == null)
+                {
+                    searchClientConfig = new SearchClientConfig
+                    {
+                        MaxAskPromptSize = 2048,
+                        MaxMatchesCount = 3,
+                        AnswerTokens = 1000,
+                        EmptyAnswer = "知识库未搜索到相关内容"
+                    };
+                }
+                var memoryBuild = new KernelMemoryBuilder()
+                .WithSearchClientConfig(searchClientConfig)
+                .WithCustomTextPartitioningOptions(new TextPartitioningOptions())
+                .WithAzureOpenAITextGeneration(new AzureOpenAIConfig()
+                {
+                    APIKey = "28df2a037e7c432a85f7892d280b99c9",
+                    Deployment = "text-embedding-ada-002",
+                    Endpoint = "https://yoyochatx.openai.azure.com/",
+                    Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+                    APIType =AzureOpenAIConfig.APITypes.TextCompletion,
+                })
+                .WithAzureOpenAITextEmbeddingGeneration(new AzureOpenAIConfig()
+                {
+                    APIKey = "28df2a037e7c432a85f7892d280b99c9",
+                    Deployment = "text-embedding-ada-002",
+                    Endpoint = "https://yoyochatx.openai.azure.com/",
+                    Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+                    APIType = AzureOpenAIConfig.APITypes.EmbeddingGeneration,
+                }).WithSimpleFileStorage(new SimpleFileStorageConfig()
+                {
+                    StorageType = FileSystemTypes.Volatile
+                }); ;
+
+                _memory = memoryBuild.Build<MemoryServerless>();
+                return _memory;
+            }
+            //else {
+            //    return _memory;
+            //}
+
+        }
+    }
+}
